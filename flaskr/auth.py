89da5932a47efaaddf397861af57e243fb685783
Tom Bauer <tbauer1898@gmail.com>
2021-05-11 00:36:36 -0500

    auth register view built

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -16,5 +16,34 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 # this app will have two blueprints - one for authentication
 # and one for blog posts
+# need to register blueprints in the __init__ file 
 
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username)
+        ).fetchone() is not None:
+        error = 'User {} is already registered'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?,?)',
+                (username, generated_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+        
+        flash(error)
+        
+    return render_template('auth/register.html')
 
